c7a6f89fe88ccea256fba13b6a8e3d1a57a94343
nicolaivinther <nicolai92@gmail.com>
2017-09-18 09:53:42 +0200

    Done

--- ExerciseW2,2.py
+++ ExerciseW2,2.py
@@ -1,17 +1,68 @@
+# -*- coding: utf-8 -*-
 import os
 import json
+import re
 from pprint import pprint
-#DU er en bussemand
+
 os.chdir("/Users/Nicolai/Dropbox/KU/10. Semester/Computational tool for big dataset")
 with open('pizza-train.json') as data_file:
     data = json.load(data_file)
 
-def get_text(key):
-    newlist = []
-    for elt in range(len(data)):
-        newlist.append(data[elt][key])
- #   newlist = map( str.lower, newlist )
-    print newlist[0:2]
-    return newlist
 
-get_text('request_text')
+
+def get_uniqe_words(key):
+    # This function will return a list cosisting of all
+    # uniqe words from every single review in the json file.
+    newstring= unicode() # Empty string
+    for elt in range(len(data)): # Iterate through all dicts
+         newstring += data[elt][key] # Append string to newstring
+    newstrlow = newstring.lower() # Turn to lowercase letters
+    newstrlow = newstrlow.replace("\n", "") # Remove new lines
+    # Remove non-aplhabetic characters but space
+    newstralpha = re.sub("[^a-zA-Z ]+", "", newstrlow)
+    newlist = newstralpha.split()
+    # Converting the list into a set() to only get the uniqe words
+    uniqeset = set(newlist)
+    # Converting set back to list
+    uniqelist = list(uniqeset)
+    return uniqelist
+#get_uniqe_words('request_text')
+
+def create_list(key):
+	# This function will return a list of lists, where each list
+	# cosists of all words in every singe review.
+    l = [] # Creating empty list to contain all reviews
+    for i in range(len(data)):
+	    l.append(data[i][key])
+	    l[i] = l[i].lower() # Make lowercase letters
+	    l[i] = l[i].replace("\n", "") # Remove new lines
+	    # Remove non-alphabetic characters bu space
+	    l[i] = re.sub("[^a-zA-Z ]+", "", l[i])
+	    # Turn string into list
+	    l[i] = l[i].split()
+    return l
+#create_list('request_text')
+
+# Call the uniqe word function, to get the list of uniqe words
+countlist = get_uniqe_words('request_text')
+# Call the create list function, to get the list of all reviews
+reviewlist = create_list('request_text')
+# Create an array with (Number of revies x Number of uniqe words)
+lists = [[] for _ in range(len(create_list('request_text')))]
+
+# Iterate through Number of reviews
+for review in range(len(reviewlist)):
+	# Iterate through Number of words
+    for elt in range(len(countlist)):
+    	# Count each uniqe words occurrences in each review
+	    count = reviewlist[review].count(countlist[elt])
+	    # Append the count to each reviews list
+	    lists[review].append(count)
+
+# Printing the result
+print len(lists) # Print number of reviews
+print len(lists[5]) # Print number of uniqe words
+# Print all word counts from words in review six that appears at least once
+for index, value in enumerate(lists[5]):
+	if value > 0:
+		print(index, value)
--- ExerciseW2.py
+++ ExerciseW2.py
@@ -1,34 +1,29 @@
 def aif(x,l):
+    """ 
+    This is a help function. It takes a character and list
+    as arguments. It then inserts the character in front of all
+    other characters in the given list, and return the list.
+    """
     for i in range(len(l)):
             l[i].insert(0, x)
     return l
-"""
-lst = [[0,0],[0,1],[1,0],[1,1]]
-lste = [[0,0],[0,1],[1,0],[1,1]]
-print aif(0,lst)
-print aif(1,lste)
 
-"""
 def bitstr(n):
-    if n == 0: return []
-    if n == 1: return [[0],[1]]
-    if n == 2: return [[0,0],[0,1],[1,0],[1,1]]
+    if n == 0: return [] # Base case of n = 0
+    if n == 1: return [[0],[1]] # Base case of n = 1
+    if n == 2: return [[0,0],[0,1],[1,0],[1,1]] # Base case of n = 2
     else:
+        # Using recursion to find the next bitstrings by taking the
+        # previous bitstring twice. First time we add "0" in front of
+        # all the elements in each list, and the second time we add "1"
+        # to all elements in each list. We do this by calling our help
+        # function "aif".
         return (aif(0, bitstr(n-1)) + aif(1, bitstr(n-1)))
 
-"""                    
-print bitstr(0)
-print bitstr(1)
-print bitstr(2)
-print bitstr(3)
-print bitstr(4)
-print bitstr(5)
-"""
-
 def printbitstring(n):
+    # Iterate though n and the bitstr function to print the result.
     for elt in bitstr(n):
         print elt
-
 
 print printbitstring(0)
 print printbitstring(1)
